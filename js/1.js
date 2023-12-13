51e1431efede64ec22ff6312ddfb9d0e2143b9f6
Maiamona Garcia <maiamona.garcia@jupiter.co.ao>
2023-12-13 10:04:04 +0100

    6

--- js/1.js
+++
@@ -1,9 +0,0 @@
-function changeColor(element) {
-    var currentColor = element.style.backgroundColor;
-    if (currentColor == "red") {
-        element.style.backgroundColor = "green";
-    }
-    else{
-        element.style.backgroundColor = "red";
-    }
-}
